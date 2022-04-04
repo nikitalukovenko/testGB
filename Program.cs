cf1276c74ccbdc086bb82388d06e8c65c3cd6348
Nikita Lukovenko <kiddylastdoor@gmail.com>
2022-04-04 16:54:45 +0300

    set the condition

--- Program.cs
+++ Program.cs
@@ -4,3 +4,7 @@
     public static void Main(string[] args)
     {
         var array = new[] {"Tom", "Sam", "Bob", "Kate", "Alice", "Sa"};
+
+           string[] newArray = Array.FindAll(array, arr => arr.Length <= 3);
+
+
